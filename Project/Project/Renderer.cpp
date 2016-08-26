3fdac345b3f1516b8a1e8d891df1e46761b6a8dc
Rajnesh <Rajnesh@LAPTOP-RAJ>
2016-08-26 19:11:10 +0100

    Actually add all changes from  the previous commit msg

--- Project/Project/Renderer.cpp
+++ Project/Project/Renderer.cpp
@@ -1,6 +1,8 @@
 #include "Renderer.h"
+#include "Debug.h"
 
-#include "memory"
+#include <memory>
+#include <assert.h>
 
 Renderer::Renderer()
 {
@@ -10,102 +12,336 @@ Renderer::Renderer()
 
 Renderer::~Renderer()
 {
+	//TD move to cleanup function
+	vkDestroyDevice(devices[0], nullptr);
+	vkDestroyInstance(instance, nullptr);
+	if (globalExtensionNames)
+		delete[] globalExtensionNames;
+	if (layerNames)
+		delete[] layerNames;
 }
 
-bool Renderer::CreateInstance()
+bool Renderer::GetInstanceLayers()
 {
 	//Get Layer count and layer properties
-	uint32_t layerPropertiesCount;
-	
 	auto err = vkEnumerateInstanceLayerProperties(&layerPropertiesCount, nullptr);
 
 	if (err != VK_SUCCESS)
 	{
-		//TODO output an error 
+		Debug::Log("Enumerate global instance layers failed", DebugLevel::Error);
 		return false;
 	}
 
+	Debug::Log(std::string("Enumerating global instance layers: ") + ToString(layerPropertiesCount) + " found");
 	if (layerPropertiesCount == 0)
 	{
 		return true;
 	}
-
-	std::unique_ptr<VkLayerProperties[]> layerPropertiesArray(new VkLayerProperties[layerPropertiesCount]);	
+	std::unique_ptr<VkLayerProperties[]> layerPropertiesArray(new VkLayerProperties[layerPropertiesCount]);
 
 	err = vkEnumerateInstanceLayerProperties(&layerPropertiesCount, layerPropertiesArray.get());
 	if (err != VK_SUCCESS)
 	{
-		//TODO output debug error
-		return false;
-	}
-
-	// Get extension count and properties
-	uint32_t extensionCount;
-	err = vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-	if (err != VK_SUCCESS)
-	{
-		//TODO output an error 
-		return false;
-	}
-
-	std::unique_ptr<VkExtensionProperties[]> extensionsArray( new VkExtensionProperties[extensionCount] );
-
-	err = vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensionsArray.get());
-	if (err != VK_SUCCESS)
-	{
-		//TODO output an error 
+		Debug::Log("Enumerate instance layers failed while retrieving layers", DebugLevel::Error);
 		return false;
 	}
 
 	//Get layer names
-	char ** layerNames = new char*[layerPropertiesCount];
+	layerNames = new char*[layerPropertiesCount];
 
 	for (uint32_t i = 0; i < layerPropertiesCount; ++i)
 	{
+		Debug::Log(std::string("Instance layer ") + ToString(i) + " : " + layerPropertiesArray[i].layerName + " Desc: " + layerPropertiesArray[i].description);
+		Debug::Log(std::string("Require Api version: ") + ToString(VK_VERSION_MAJOR(layerPropertiesArray[i].specVersion)) + '.' + ToString(VK_VERSION_MINOR(layerPropertiesArray[i].specVersion)) + '.' + ToString(VK_VERSION_PATCH(layerPropertiesArray[i].specVersion)) );
 		size_t nameLength = strlen(layerPropertiesArray[i].layerName) + 1;
 		layerNames[i] = new char[nameLength];
 		strcpy_s(layerNames[i], nameLength, layerPropertiesArray[i].layerName);
 	}
 
+	return true;
+}
+
+bool Renderer::GetInstanceExtensions(const char * layername, uint32_t &extensionCount, char**& extensionNames)
+{
+	if (layername)
+	{
+		Debug::Log(std::string("Enumerate ") + layername + " instance extensions");
+	}
+	else
+	{
+		Debug::Log("Enumerate global instance extensions");
+	}
+	// Get extension count and properties
+	auto err = vkEnumerateInstanceExtensionProperties(layername, &extensionCount, nullptr);
+	if (err != VK_SUCCESS)
+	{
+		Debug::Log("Enumerate instance extensions failed", DebugLevel::Error);
+		return false;
+	}
+	Debug::Log(std::string("Enumerating instance extensions: ") + ToString(extensionCount) + " found");
+
+	std::unique_ptr<VkExtensionProperties[]> extensionsArray(new VkExtensionProperties[extensionCount]);
+
+	err = vkEnumerateInstanceExtensionProperties(layername, &extensionCount, extensionsArray.get());
+	if (err != VK_SUCCESS)
+	{
+		Debug::Log("Enumerate instance extensions failed while retrieving extensions", DebugLevel::Error);
+		return false;
+	}
 
 	//Get extension names
-	char** extensionNames = new char*[extensionCount];
+	extensionNames = new char*[extensionCount];
 
 	for (uint32_t i = 0; i < extensionCount; ++i)
 	{
+		Debug::Log(std::string("Instance extension ") + ToString(i) + " : " + extensionsArray[i].extensionName);
 		size_t nameLength = strlen(extensionsArray[i].extensionName) + 1;
 		extensionNames[i] = new char[nameLength];
 		strcpy_s(extensionNames[i], nameLength, extensionsArray[i].extensionName);
 	}
-	
-	//Create instance
 
+	return true;
+}
+
+bool Renderer::CreateInstance()
+{
+	//Create instance
 	VkApplicationInfo appInfo {};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName = "Test";
+	appInfo.pApplicationName = "Rkj Vulkan Sample app";
+	appInfo.pEngineName = "Rkj Vulkan Sample app";
 	appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 8); // VK_API_VERSION; is deprecated
 
-	VkInstanceCreateInfo createInfo {};
-	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	createInfo.pApplicationInfo = &appInfo;
-	createInfo.enabledLayerCount = layerPropertiesCount;
-	createInfo.ppEnabledLayerNames = layerNames;
-	createInfo.enabledExtensionCount = extensionCount;
-	createInfo.ppEnabledExtensionNames = extensionNames;
+	VkInstanceCreateInfo instanceCreateInfo {};
+	instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceCreateInfo.pApplicationInfo = &appInfo;
+	instanceCreateInfo.enabledLayerCount = layerPropertiesCount - 1;
+	instanceCreateInfo.ppEnabledLayerNames = layerNames;
+	instanceCreateInfo.enabledExtensionCount = globalExtensionCount;
+	instanceCreateInfo.ppEnabledExtensionNames = globalExtensionNames;
 
-	err = vkCreateInstance(&createInfo, nullptr, &instance);
+	auto err = vkCreateInstance(&instanceCreateInfo, nullptr, &instance);
+	if (err != VK_SUCCESS)
+	{
+		Debug::Log("Create Vulkan instance failed");
+		return false;
+	}
+
+	return true;
+}
+
+bool Renderer::EnumerateDevices()
+{
+	assert(instance && "VkInstance not initialised");	//TODO: improve error handling
+
+	auto err = vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+	if (err != VK_SUCCESS)
+	{
+		Debug::Log("Enumerate physical devices failed", DebugLevel::Error);
+		return false;
+	}
+
+	Debug::Log(std::string("Enumerating physical devices: ") + ToString(deviceCount) + " found");
+
+	gpus = new VkPhysicalDevice[deviceCount];
+
+	err = vkEnumeratePhysicalDevices(instance, &deviceCount, gpus);
+
+	if (err != VK_SUCCESS)
+	{
+		Debug::Log("Enumerate physical devices: Failed to retrieve devices", DebugLevel::Error);
+		return false;
+	}
+
+	for (uint32_t i = 0; i < deviceCount; ++i)
+	{
+		VkPhysicalDeviceProperties properties;
+		vkGetPhysicalDeviceProperties(gpus[i], &properties);
+		Debug::Log(std::string("Device Name ") + properties.deviceName);
+	}
+
+	VkPhysicalDeviceProperties properties;
+	vkGetPhysicalDeviceProperties(gpus[0], &properties);
+
+	//Get queue data
+	uint32_t queueCount;
+	vkGetPhysicalDeviceQueueFamilyProperties(gpus[0], &queueCount, nullptr);
+
+	Debug::Log(std::string("Using default device: ") + properties.deviceName + "Which has " + ToString(queueCount) + " queues");
+	Debug::Log("Searching for first graphics capable queue");
+
+
+	auto queueProperties = new VkQueueFamilyProperties[queueCount];
+
+	uint32_t queueFamilyIndex = -1;
+
+	vkGetPhysicalDeviceQueueFamilyProperties(gpus[0], &queueCount, queueProperties);
+	for (uint32_t i = 0; i < queueCount; ++i)
+	{
+		if (queueProperties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		{
+			queueFamilyIndex = i;
+		}
+	}
+
+	//Create device
+	float queue_priorities[1] = { 0.0f };
+	VkDeviceQueueCreateInfo queue = {};
+	queue.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queue.queueFamilyIndex = queueFamilyIndex;
+	queue.queueCount = 1;
+	queue.pQueuePriorities = queue_priorities;
+
+	VkDeviceCreateInfo deviceCreateInfo = {};
+
+	deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	deviceCreateInfo.pQueueCreateInfos = &queue;
+
+	devices = new VkDevice[deviceCount];
+
+	Debug::Log("Creating default logical device");
+
+	err = vkCreateDevice(gpus[0], &deviceCreateInfo, nullptr, &devices[0]);
+
+	return true;
+}
+
+bool Renderer::CreateAppWindow()
+{
+	WNDCLASSEX wc = {};
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.hInstance = winAppInstance;
+	//wc.lpfnWndProc = 
+	wc.lpszClassName = L"Classname";
+
+	if (!RegisterClassEx(&wc))
+	{
+		return false;
+	}
+
+	wnd = CreateWindowEx(0, wc.lpszClassName, L"Appname", WS_VISIBLE, 0, 0, 640, 480, nullptr, nullptr, wc.hInstance, nullptr);
+
+	if (wnd == nullptr)
+	{
+		return false;
+	}
+	
+
+	return true;
+}
+
+bool Renderer::CreateSwapchain()
+{
+	VkWin32SurfaceCreateInfoKHR surfaceInfo{};
+	surfaceInfo.sType		= VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+	surfaceInfo.hinstance	= winAppInstance;
+	surfaceInfo.pNext		= wnd;
+
+	auto err = vkCreateWin32SurfaceKHR(instance, &surfaceInfo, nullptr, &surface);
+
+	//Get surface format
+	uint32_t formatCount;
+	err = vkGetPhysicalDeviceSurfaceFormatsKHR(gpus[0], surface, &formatCount, nullptr);
+
 	if (err != VK_SUCCESS)
 	{
 		//TODO output an error 
 		return false;
 	}
 
-	//Create device
+	auto surfaceFormats = new VkSurfaceFormatKHR[formatCount];
+	err = vkGetPhysicalDeviceSurfaceFormatsKHR(gpus[0], surface, &formatCount, surfaceFormats);
+
+	if (err != VK_SUCCESS)
+	{
+		//TODO output an error 
+		return false;
+	}
+
+	if (formatCount == 1 && surfaceFormats[0].format == VK_FORMAT_UNDEFINED)
+	{
+		currentFormat = VK_FORMAT_B8G8R8A8_UNORM;
+	}
+	else
+	{
+		currentFormat = surfaceFormats[0].format;
+	}
+
+	//Get Present mode?
+	//Get and graphics bit
+
+
+	//Create Swapchain
+	VkSwapchainCreateInfoKHR swapchainCreateInfo {};
+	swapchainCreateInfo.sType			= VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	swapchainCreateInfo.surface			= surface;
+	swapchainCreateInfo.imageFormat		= currentFormat;
+	swapchainCreateInfo.minImageCount	= 1;
+	swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+	swapchainCreateInfo.imageExtent.height	= height;
+	swapchainCreateInfo.imageExtent.width	= width;
+
+	swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	swapchainCreateInfo.imageArrayLayers = 1;
+//	swapchainCreateInfo.preTransform = preTransform;
+//	swapchainCreateInfo.presentMode = swapchainPresentMode;
+
+	err = vkCreateSwapchainKHR(devices[0], &swapchainCreateInfo, nullptr, &swapchain);
+	if (err != VK_SUCCESS)
+	{
+		//TODO output an error 
+		return false;
+	}
 
 	return true;
 }
 
-bool Renderer::Init()
+bool Renderer::CreateTri()
 {
-	return CreateInstance();
+	float vertices[] = {
+		0, -1, 0,
+		-1, 1, 0,
+		1, 1,  0
+	};
+
+	VkBufferCreateInfo vbInfo = {};
+	vbInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	vbInfo.size = sizeof(vertices);
+	vbInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+	
+	VkMemoryAllocateInfo vbAllocInfo = {};
+	vbAllocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+
+	VkBuffer vertexBuffer;
+
+	auto err = vkCreateBuffer(devices[0], &vbInfo, nullptr, &vertexBuffer);
+	if (err != VK_SUCCESS)
+	{
+		//TODO output an error 
+		return false;
+	}
+
+	VkMemoryRequirements memRequirements;
+	
+	vkGetBufferMemoryRequirements(devices[0], vertexBuffer, &memRequirements);
+
+
+	return true;
+}
+
+
+bool Renderer::Init(HINSTANCE hInstance)
+{
+	winAppInstance = hInstance;
+	//TODO Error checking
+	GetInstanceLayers();
+	GetInstanceExtensions();
+	CreateInstance();
+	EnumerateDevices();
+
+	CreateAppWindow();
+	CreateSwapchain();
+
+	return true;
 }
--- Project/Project/Renderer.h
+++ Project/Project/Renderer.h
@@ -1,19 +1,54 @@
 #pragma once
-
-#include "vulkan.h"
+#define VK_USE_PLATFORM_WIN32_KHR
+//#include "vulkan\vk_cpp.hpp"
+#include "vulkan\vulkan.h"
 
 class Renderer
 {
 private:
-	VkInstance instance;
+	int					width	{ 640 };
+	int					height	{ 480 };
 
 protected:
+	//instance layers
+	uint32_t			layerPropertiesCount{};
+	char**				layerNames{ nullptr };
+	bool				GetInstanceLayers();
 
-	bool CreateInstance();
+	//instance extensions
+	uint32_t			globalExtensionCount {};
+	char**				globalExtensionNames {nullptr};
+	bool				GetInstanceExtensions(const char * layername, uint32_t &extensionCount, char**& extensionNames);
+	bool				GetInstanceExtensions()
+	{
+		return GetInstanceExtensions(nullptr, globalExtensionCount, globalExtensionNames);
+	}
+
+	VkInstance			instance { nullptr };
+	bool				CreateInstance();
+
+	//devices
+	uint32_t			deviceCount{};
+	VkPhysicalDevice*	gpus { nullptr };
+	VkDevice*			devices{ nullptr };
+	bool				EnumerateDevices();
+
+	HINSTANCE			winAppInstance;
+	HWND				wnd;
+	bool CreateAppWindow();
+
+	VkSurfaceKHR		surface { nullptr };
+	VkFormat			currentFormat;
+	VkSwapchainKHR		swapchain{ nullptr };
+	bool CreateSwapchain();
+
+
+	bool CreateTri();
+
 public:
 	Renderer();
 	~Renderer();
 
-	bool Init();
+	bool Init(HINSTANCE hInstance);
 };
 
